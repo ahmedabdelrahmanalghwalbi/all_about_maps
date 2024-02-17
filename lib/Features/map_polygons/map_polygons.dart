cf8e2f051b73796eadce312ab4e7306f64845e58
ahmedabdelrahmanalghwalbi <https://github.com/ahmedabdelrahmanalghwalbi>
2024-02-17 18:39:04 +0200

    #polygons

--- lib/Features/map_polygons/map_polygons.dart
+++ lib/Features/map_polygons/map_polygons.dart
@@ -10,6 +10,11 @@ class MapPolygons extends StatefulWidget {
 
 class _MapPolygonsState extends State<MapPolygons> {
   Set<Marker> markers = {};
+  List<LatLng> polygonPoints = [
+    const LatLng(37.7749, -122.4194),
+    const LatLng(37.7850, -122.4074),
+    const LatLng(37.7739, -122.4037),
+  ];
   late GoogleMapController mapController;
   CameraPosition cameraPosition = const CameraPosition(
     target: LatLng(37.4219999, -122.0840575),
@@ -27,7 +32,7 @@ class _MapPolygonsState extends State<MapPolygons> {
             )),
         centerTitle: true,
         title: const Text(
-          "Geocoding",
+          "Polygons",
           style: TextStyle(color: Colors.white, fontSize: 14),
         ),
         backgroundColor: Colors.red,
@@ -37,7 +42,8 @@ class _MapPolygonsState extends State<MapPolygons> {
           const SizedBox(
             height: 12,
           ),
-          const Text("Current Location After Geocoding :-"),
+          const Text(
+              "Make Polygon , darg and drop markers to change your polygon"),
           const SizedBox(
             height: 12,
           ),
@@ -47,88 +53,71 @@ class _MapPolygonsState extends State<MapPolygons> {
           ),
           Expanded(
             child: GoogleMap(
+              markers: markers,
+              polygons: {
+                Polygon(
+                  polygonId: const PolygonId('polygon_1'),
+                  points: polygonPoints,
+                  fillColor: Colors.black, // Fill color
+                  strokeColor: Colors.blue, // Stroke color
+                  strokeWidth: 100, // Stroke width
+                  geodesic: true, // Geodesic or straight line
+                ),
+              },
               onLongPress: (LatLng latLng) {
                 // Do something when the map is long pressed
               },
               onTap: (LatLng latLng) {
                 // Do something when the map is tapped
-                setState(() {
-                  markers.add(Marker(
-                      markerId:
-                          MarkerId("${latLng.latitude + latLng.longitude}"),
-                      //Geographical location of the marker.
-                      position: latLng,
-                      // The opacity of the marker, between 0.0 and 1.0 inclusive.
-                      alpha: 0.5,
-                      // True if the marker is visible.
-                      visible: true,
-                      // Callbacks to receive tap events for markers placed on this map.
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext ctx) {
-                            return AlertDialog(
-                              title: Text(
-                                  'Hi Im Marker with Id ${latLng.latitude + latLng.longitude}'),
-                              content: Text(
-                                  "i'm marker who placed in ${latLng.latitude} -- ${latLng.longitude}"),
-                              actions: [
-                                TextButton(
-                                  onPressed: () {
-                                    // Close the dialog when this button is pressed
-                                    Navigator.of(ctx).pop();
-                                  },
-                                  child: const Text('Close'),
-                                ),
-                              ],
-                            );
-                          },
+                markers.add(Marker(
+                  markerId: MarkerId("${latLng.latitude + latLng.longitude}"),
+                  //Geographical location of the marker.
+                  position: latLng,
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext ctx) {
+                        return AlertDialog(
+                          title: Text(
+                              'Hi Im Marker with Id ${latLng.latitude + latLng.longitude}'),
+                          content: Text(
+                              "i'm marker who placed in ${latLng.latitude} -- ${latLng.longitude}"),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                // Close the dialog when this button is pressed
+                                Navigator.of(ctx).pop();
+                              },
+                              child: const Text('Close'),
+                            ),
+                          ],
                         );
                       },
-                      //The window is displayed when the marker is tapped.
-                      infoWindow: InfoWindow(
-                        title: "hi",
-                        onTap: () {},
-                        snippet: "im the marker !!!",
-                      ),
-                      //True if the marker is draggable by user touch events.
-                      draggable: true,
-                      //Signature reporting the new [LatLng] during the drag event.
-                      onDrag: (LatLng newLatlng) {
-                        markers.map((marker) {
-                          if (marker.markerId.value ==
-                              "${latLng.latitude + latLng.longitude}") {
-                            setState(() {
-                              marker =
-                                  marker.copyWith(positionParam: newLatlng);
-                            });
-                          }
+                    );
+                  },
+                  draggable: true,
+                  //Signature reporting the new [LatLng] during the drag event.
+                  onDrag: (LatLng newLatlng) {
+                    markers.map((marker) {
+                      if (marker.markerId.value ==
+                          "${latLng.latitude + latLng.longitude}") {
+                        setState(() {
+                          marker = marker.copyWith(positionParam: newLatlng);
                         });
-                      },
-                      // Signature reporting the new [LatLng] at the start of a drag event.
-                      onDragStart: (latlang) {},
-                      // Signature reporting the new [LatLng] at the start of a drag event.
-                      onDragEnd: (latlng) {},
-                      //True if the marker is rendered flatly against the surface of the Earth, so that it will rotate and tilt along with map camera movements.
-                      flat: true,
-                      // Rotation of the marker image in degrees clockwise from the [anchor] point. start from 1 degree=> 360 degrees
-                      rotation: 40,
-                      // A description of the bitmap used to draw the marker icon.
-                      //change marker icon to be an image from assetrs
-                      // icon: BitmapDescriptor.fromAssetImage(
-                      //   //configration of image
-                      //   ImageConfiguration(),
-                      //   //path of the image
-                      //    "image path")
-                      //chnage marker icons from bytes
-                      // icon: BitmapDescriptor.fromBytes(byteData)
-                      //default Marker
-                      icon: BitmapDescriptor.defaultMarker,
-                      //True if the marker icon consumes tap events. If not, the map will perform default tap handling by centering the map on the marker and displaying its info window.
-                      consumeTapEvents: true,
-                      // The icon image point that will be placed at the [position] of the marker.
-                      anchor: const Offset(0.0, 0.0)));
-                });
+                      }
+                    });
+                  },
+                  // Signature reporting the new [LatLng] at the start of a drag event.
+                  onDragStart: (latlang) {},
+                  // Signature reporting the new [LatLng] at the start of a drag event.
+                  onDragEnd: (latlng) {
+                    setState(() {
+                      polygonPoints.add(latLng);
+                    });
+                  },
+                ));
+
+                setState(() {});
               },
               onCameraMove: (CameraPosition position) {
                 // Do something when the camera position changes
@@ -139,6 +128,57 @@ class _MapPolygonsState extends State<MapPolygons> {
               onMapCreated: (GoogleMapController controller) {
                 // Do something when the map is created
                 mapController = controller;
+                //set Markers on Polygon vertics
+                polygonPoints.map((latlng) {
+                  markers.add(Marker(
+                    markerId: MarkerId("${latlng.latitude + latlng.longitude}"),
+                    //Geographical location of the marker.
+                    position: latlng,
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext ctx) {
+                          return AlertDialog(
+                            title: Text(
+                                'Hi Im Marker with Id ${latlng.latitude + latlng.longitude}'),
+                            content: Text(
+                                "i'm marker who placed in ${latlng.latitude} -- ${latlng.longitude}"),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  // Close the dialog when this button is pressed
+                                  Navigator.of(ctx).pop();
+                                },
+                                child: const Text('Close'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    draggable: true,
+                    //Signature reporting the new [LatLng] during the drag event.
+                    onDrag: (LatLng newLatlng) {
+                      markers.map((marker) {
+                        if (marker.markerId.value ==
+                            "${newLatlng.latitude + newLatlng.longitude}") {
+                          setState(() {
+                            marker = marker.copyWith(positionParam: newLatlng);
+                          });
+                        }
+                      });
+                    },
+                    // Signature reporting the new [LatLng] at the start of a drag event.
+                    onDragStart: (latlang) {},
+                    // Signature reporting the new [LatLng] at the start of a drag event.
+                    onDragEnd: (latlng) {
+                      setState(() {
+                        polygonPoints.add(latlng);
+                      });
+                    },
+                  ));
+                });
+                setState(() {});
               },
               initialCameraPosition: cameraPosition,
             ),
