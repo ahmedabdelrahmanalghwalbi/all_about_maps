4166942c109729f186dd10b90003c1e14152dc5a
ahmedabdelrahmanalghwalbi <https://github.com/ahmedabdelrahmanalghwalbi>
2024-02-17 17:46:20 +0200

    #maps_circles

--- lib/Features/map_circles/map_circles.dart
+++ lib/Features/map_circles/map_circles.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:uuid/uuid.dart';
 
 class MapsCircles extends StatefulWidget {
   const MapsCircles({super.key});
@@ -9,11 +10,11 @@ class MapsCircles extends StatefulWidget {
 }
 
 class _MapsCirclesState extends State<MapsCircles> {
-  Set<Marker> markers = {};
+  Set<Circle> circles = {};
   late GoogleMapController mapController;
   CameraPosition cameraPosition = const CameraPosition(
     target: LatLng(37.4219999, -122.0840575),
-    zoom: 7,
+    zoom: 12,
   );
   @override
   Widget build(BuildContext context) {
@@ -27,7 +28,7 @@ class _MapsCirclesState extends State<MapsCircles> {
             )),
         centerTitle: true,
         title: const Text(
-          "Geocoding",
+          "Google Maps Circles",
           style: TextStyle(color: Colors.white, fontSize: 14),
         ),
         backgroundColor: Colors.red,
@@ -37,7 +38,7 @@ class _MapsCirclesState extends State<MapsCircles> {
           const SizedBox(
             height: 12,
           ),
-          const Text("Current Location After Geocoding :-"),
+          const Text("Tap on Map To Add Circles"),
           const SizedBox(
             height: 12,
           ),
@@ -47,87 +48,22 @@ class _MapsCirclesState extends State<MapsCircles> {
           ),
           Expanded(
             child: GoogleMap(
+              circles: circles,
               onLongPress: (LatLng latLng) {
                 // Do something when the map is long pressed
               },
               onTap: (LatLng latLng) {
                 // Do something when the map is tapped
                 setState(() {
-                  markers.add(Marker(
-                      markerId:
-                          MarkerId("${latLng.latitude + latLng.longitude}"),
-                      //Geographical location of the marker.
-                      position: latLng,
-                      // The opacity of the marker, between 0.0 and 1.0 inclusive.
-                      alpha: 0.5,
-                      // True if the marker is visible.
-                      visible: true,
-                      // Callbacks to receive tap events for markers placed on this map.
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext ctx) {
-                            return AlertDialog(
-                              title: Text(
-                                  'Hi Im Marker with Id ${latLng.latitude + latLng.longitude}'),
-                              content: Text(
-                                  "i'm marker who placed in ${latLng.latitude} -- ${latLng.longitude}"),
-                              actions: [
-                                TextButton(
-                                  onPressed: () {
-                                    // Close the dialog when this button is pressed
-                                    Navigator.of(ctx).pop();
-                                  },
-                                  child: const Text('Close'),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                      //The window is displayed when the marker is tapped.
-                      infoWindow: InfoWindow(
-                        title: "hi",
-                        onTap: () {},
-                        snippet: "im the marker !!!",
-                      ),
-                      //True if the marker is draggable by user touch events.
-                      draggable: true,
-                      //Signature reporting the new [LatLng] during the drag event.
-                      onDrag: (LatLng newLatlng) {
-                        markers.map((marker) {
-                          if (marker.markerId.value ==
-                              "${latLng.latitude + latLng.longitude}") {
-                            setState(() {
-                              marker =
-                                  marker.copyWith(positionParam: newLatlng);
-                            });
-                          }
-                        });
-                      },
-                      // Signature reporting the new [LatLng] at the start of a drag event.
-                      onDragStart: (latlang) {},
-                      // Signature reporting the new [LatLng] at the start of a drag event.
-                      onDragEnd: (latlng) {},
-                      //True if the marker is rendered flatly against the surface of the Earth, so that it will rotate and tilt along with map camera movements.
-                      flat: true,
-                      // Rotation of the marker image in degrees clockwise from the [anchor] point. start from 1 degree=> 360 degrees
-                      rotation: 40,
-                      // A description of the bitmap used to draw the marker icon.
-                      //change marker icon to be an image from assetrs
-                      // icon: BitmapDescriptor.fromAssetImage(
-                      //   //configration of image
-                      //   ImageConfiguration(),
-                      //   //path of the image
-                      //    "image path")
-                      //chnage marker icons from bytes
-                      // icon: BitmapDescriptor.fromBytes(byteData)
-                      //default Marker
-                      icon: BitmapDescriptor.defaultMarker,
-                      //True if the marker icon consumes tap events. If not, the map will perform default tap handling by centering the map on the marker and displaying its info window.
-                      consumeTapEvents: true,
-                      // The icon image point that will be placed at the [position] of the marker.
-                      anchor: const Offset(0.0, 0.0)));
+                  circles.add(Circle(
+                      circleId: CircleId(const Uuid().v4()),
+                      center: latLng,
+                      radius: 5000, // Radius in meters
+                      fillColor: Colors.blue.withOpacity(0.3), // Fill color
+                      strokeWidth: 2, // Stroke width
+                      strokeColor: Colors.blue,
+                      onTap: () {},
+                      visible: true)); // Stroke color));
                 });
               },
               onCameraMove: (CameraPosition position) {
